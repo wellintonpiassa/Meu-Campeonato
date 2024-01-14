372d609c22fb1b025ec6f39a09da8e3ab52ac27c
Wellinton Piassa <wellintonpiassa@hotmail.com>
2024-01-14 18:07:23 -0300

    fix: Making name of team unique

--- backend/database/migrations/2024_01_13_060535_create_teams_table.php
+++ backend/database/migrations/2024_01_13_060535_create_teams_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable(false);
+            $table->string('name')->unique(false);
         });
     }
 
