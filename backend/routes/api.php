505d33f2a4533baa7790bc32174fb27944a11f16
Wellinton Piassa <wellintonpiassa@hotmail.com>
2024-01-14 18:06:38 -0300

    feat: backend implementation for teams

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\AuthController;
+use App\Http\Controllers\TeamController;
 
+// Rotas de autenticação e criação de usuários
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function(){
   Route::get('user', [AuthController::class, 'user']);
   Route::post('logout', [AuthController::class, 'logout']);
 });
+
+// Rotas envolvendo times
+Route::middleware('auth:sanctum')->group(function(){
+  Route::get('teams/all', [TeamController::class, 'all']);
+  Route::post('teams/new', [TeamController::class, 'store']);
+});
+
